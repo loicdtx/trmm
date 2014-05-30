8f2aecd5e42be9bb05d68d28a273b67c3e967b91
unknown <dutri001@L0116984.wurnet.nl>
2014-05-30 12:03:12 +0200

    Added 3 by 3 pixel TRMM brick/stack of daily precipitation estimates

--- sandbox.R
+++ sandbox.R
@@ -5,7 +5,15 @@ library(bfast)
 data(wichita)
 head(wichita)
 
+# Build a TRMM dataset
+dir <- 'F:/RS/test/trmm/'
+list <- list.files(dir, pattern='^.*\\.(nc)', full.names=TRUE)
+stack <- stack(list)
 
+subStack <- crop(stack, e)
+
+subStack2 <- setZ(x=subStack, z=trmm2date(list))
+daily <- subStack2
 
 
 
