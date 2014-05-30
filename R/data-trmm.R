0f14fec094729a728f03feacba792d8c7f3adc98
unknown <dutri001@L0116984.wurnet.nl>
2014-05-30 13:45:01 +0200

    Documented rasterStack added to test data

---
+++ R/data-trmm.R
@@ -0,0 +1,11 @@
+#' TRMM rasterStack 
+#'
+#' RasterStack of 3 by 3 pixels of TRMM daily product from 1999 to end November 2013. Time is written to the z dimension.
+#'
+#' @docType data
+#' @name daily
+#' @usage daily
+#' @format A \code{\link{RasterStack}}
+#' @examples
+#' daily
+NULL
